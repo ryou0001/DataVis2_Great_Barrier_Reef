505f4c3a72d5732906d0b61fb7ce3064bd9fe747
Rebecca Young <r.j.young0@gmail.com>
2025-10-17 02:31:00 +1100

    Add structure to webpage

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,25 +1,31 @@
+//Residents
 var map_overview = "vegalite_files/overview.vg.json";
 
 var map_NRM_overview = "vegalite_files/map_NRM_overview.vg.json";
-
-var tourist_origins = "vegalite_files/tourist_origin.vg.json"
-
-var watertemp_scatterplot = "vegalite_files/water_temp_scatterplot.vg.json"
+var resident_financial_bar = "vegalite_files/resident_financial_bar.vg.json"
+var resident_visits_heatmap = "vegalite_files/resident_visits_heatmap.vg.json"
+var resident_value_histogram = "vegalite_files/resident_value_histogram_square.vg.json"
 
 var resident_word_cloud = "vegalite_files/resident_word_cloud.vg.json"
-
 var resident_word_bar = "vegalite_files/resident_word_bar.vg.json"
 
+
+//Climate Change
+var watertemp_scatterplot = "vegalite_files/water_temp_scatterplot.vg.json"
 var map_resident_threats_climate = "vegalite_files/map_resident_threats_climate.vg.json"
+
+
+//Mining
 var map_resident_threats_mining = "vegalite_files/map_resident_threats_mining.vg.json"
+var mining_jobs_donut = "vegalite_files/mining_jobs_donut.vg.json"
+var mining_jobs_bar = "vegalite_files/mining_jobs_bar.vg.json"
+
+
+//Tourism
 var map_resident_threats_tourism = "vegalite_files/map_resident_threats_tourism.vg.json"
-
-
-var resident_financial_bar = "vegalite_files/resident_financial_bar.vg.json"
-
-var resident_visits_heatmap = "vegalite_files/resident_visits_heatmap.vg.json"
-
-var resident_value_histogram = "vegalite_files/resident_value_histogram.vg.json"
+var tourist_origins = "vegalite_files/tourist_origin.vg.json"
+var tourist_numbers_line = "vegalite_files/tourist_numbers_line.vg.json"
+var tourist_jobs_donut = "vegalite_files/tourist_jobs_donut.vg.json"
 
 
 vegaEmbed("#map_overview", map_overview).then(function(result) {
@@ -56,4 +62,16 @@ vegaEmbed("#resident_visits_heatmap", resident_visits_heatmap).then(function(res
 }).catch(console.error);
 
 vegaEmbed("#resident_value_histogram", resident_value_histogram).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#tourist_numbers_line", tourist_numbers_line).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#tourist_jobs_donut", tourist_jobs_donut).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#mining_jobs_donut", mining_jobs_donut).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#mining_jobs_bar", mining_jobs_bar).then(function(result) {
 }).catch(console.error);
